d6752ad9362c27d4c5fb724dbb9e5fdcd5fce5db
Serban Iordache <iordache@hotmail.com>
2017-12-15 22:31:56 +0100

    initial implementation

--- src/main/java/org/beryx/textio/multiterm/MultiTerm.java
+++ src/main/java/org/beryx/textio/multiterm/MultiTerm.java
@@ -15,8 +15,52 @@
  */
 package org.beryx.textio.multiterm;
 
+import org.beryx.textio.TextIO;
+import org.beryx.textio.swing.SwingTextTerminal;
+
+import java.awt.*;
+
 public class MultiTerm {
 	public static void main(String[] args) {
-		System.out.println("Hello from MultiTerm!");
+		SwingTextTerminal mainTerm = new SwingTextTerminal();
+		mainTerm.init();
+		TextIO mainTextIO = new TextIO(mainTerm);
+
+        mainTerm.println("######################");
+        mainTerm.println("### Mega Simulator ###");
+        mainTerm.println("######################\n\n");
+
+        while(true) {
+            mainTerm.setBookmark("mainLoop");
+            String user = mainTextIO.newStringInputReader().read("Username");
+            String device = mainTextIO.newStringInputReader()
+                    .withNumberedPossibleValues("BTS-514 (MG)", "CryoMax Beta", "BScan T-131")
+                    .read("Choose the simulated device");
+
+
+            SwingTextTerminal childTerm = new SwingTextTerminal();
+            childTerm.init();
+            TextIO childTextIO = new TextIO(childTerm);
+            // Place the child terminal next to the main terminal:
+            Rectangle mainBounds = mainTerm.getFrame().getBounds();
+            childTerm.getFrame().setLocation(mainBounds.x + mainBounds.width + 10, mainBounds.y);
+            childTerm.getFrame().setPreferredSize(new Dimension(720, mainBounds.height));
+
+            // Don't allow users to close the child terminal:
+            childTerm.registerUserInterruptHandler(term -> {}, false);
+
+            childTerm.println(device + " successfully initialized.\n");
+            childTerm.println("Hello, " + user + "!\n");
+            int age = childTextIO.newIntInputReader().read("How old are you?");
+            childTerm.println("If you exercise more, you will be very healthy on your " + (age + 1) + " birthday.");
+            childTextIO.newStringInputReader().withMinLength(0).read("\nPress enter to shut down the " + device + "... ");
+
+            childTerm.dispose();
+
+            if(!mainTextIO.newBooleanInputReader().withDefaultValue(true).read("\nRun again?")) break;
+
+            mainTerm.resetToBookmark("mainLoop");
+        }
+        mainTerm.dispose();
 	}
 }
